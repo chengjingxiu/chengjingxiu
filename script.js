a940c327519ee455ca48b1c980520fd6fc96b04b
jingxiu96 <cjingxiu@gmail.com>
2020-11-10 11:02:54 +0000

    CS1 - EVG

--- script.js
+++ script.js
@@ -25,11 +25,18 @@ $(document).ready(function() {
     };
 });
 
-// When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar //
+
+// Hamburger Menu //
+$(".hamburger").on("click", function(e) {
+    $(".hamburger").toggleClass("is-active");
+    $(".right-nav").toggleClass("hide");
+  });
+
+// When the user scrolls down, hide the top navbar. When the user scrolls up, show the top navbar //
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) { // hide only when user scrolls certain distance)
+  if (document.documentElement.scrollTop > 60) { // hide only when user scrolls certain distance)
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0px";
     } else {
@@ -39,8 +46,26 @@ window.onscroll = function() {
     }   
 }
 
-// Hamburger Menu //
-$(".hamburger").on("click", function(e) {
-    $(".hamburger").toggleClass("is-active");
-    $(".right-nav").toggleClass("hide");
-  });
+// Side Navbar //
+var topofDiv = $(".summary-background").offset().top; //gets offset of header
+var height = $(".summary-background").outerHeight(); //gets height of header
+
+$(window).scroll(function(){
+    var windscroll = $(window).scrollTop();
+
+    if (windscroll > (topofDiv + height-100)) {        // show when user reaches Overview 
+       $(".sidenav").fadeIn();                      
+
+       $('.case-study-page').each(function(i) {      // highlight active portion
+            if ($(this).position().top <= windscroll + 120) {
+                $('.sidenav a.active').removeClass('active');
+                $('.sidenav a').eq(i).addClass('active');
+            }
+        });
+    }
+
+    else{                                  // hide otherwise
+        $(".sidenav").fadeOut();
+        $(".sidenav a.active").removeClass('active');
+    }
+});
